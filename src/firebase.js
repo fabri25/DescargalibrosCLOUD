6d3c6c9990159383ed7955dd27a1c8b0ddadfcc9
fabri25 <119368535+fabri25@users.noreply.github.com>
2023-11-06 01:09:20 -0600

    3

--- src/firebase.js
+++ src/firebase.js
@@ -1,35 +1,42 @@
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const { initializeApp, credential } = require('firebase-admin');
+require('dotenv').config();
 
-// Crea un cliente de Secret Manager
+const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
 const client = new SecretManagerServiceClient();
 
-// Función para obtener las credenciales de Firebase desde Secret Manager
 async function getFirebaseCredentials() {
-  const [version] = await client.accessSecretVersion({
-    name: 'projects/firebase-service-account/secrets/firebase-service-account/versions/latest',
-  });
-
-  const credentials = JSON.parse(version.payload.data.toString());
-  return credentials;
-}
-
-// Función para inicializar la aplicación Firebase
-async function initializeFirebaseApp() {
-  try {
-    const firebaseCredentials = await getFirebaseCredentials();
-    initializeApp({
-      credential: credential.cert(firebaseCredentials),
+  if (process.env.NODE_ENV === 'production') {
+    // En producción, obtén las credenciales de Secret Manager
+    const [version] = await client.accessSecretVersion({
+      name: 'projects/descargalibros1/secrets/firebase-service-account/versions/latest',
     });
-  } catch (error) {
-    console.error('Error initializing Firebase Admin SDK:', error);
-    // Manejar el error adecuadamente
+
+    const credentials = JSON.parse(version.payload.data.toString('utf8'));
+    return cert(credentials);
+  } else {
+    // En desarrollo, usa las credenciales locales
+    // Asegúrate de que tu archivo .env local tenga la variable GOOGLE_APPLICATION_CREDENTIALS
+    // que apunte al archivo de credenciales de Firebase descargado
+    const {initializeApp, applicationDefault} =  require("firebase-admin/app")
+    const {getFirestore} =  require("firebase-admin/firestore");
+    initializeApp({
+        credential: applicationDefault()
+    })
+    const db = getFirestore();
+    module.exports = {
+    db,
+    };
   }
 }
 
-// Llama a la función para inicializar Firebase
-initializeFirebaseApp();
+initializeApp({
+    credential: applicationDefault()
+})
+const db = getFirestore();
+module.exports = {
+db,
+};
 
-// Exporta las funciones de Firebase si las necesitas en otros archivos
-const db = getFirestore(); // Por ejemplo, para Firestore
-module.exports = { db };
+
